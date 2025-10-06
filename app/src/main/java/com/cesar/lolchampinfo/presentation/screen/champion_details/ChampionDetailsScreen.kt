eb9e74bf50562f57077957da2a78f87360c005d7
Cesar <75807988+cesarguillermo@users.noreply.github.com>
2025-10-06 16:50:38 +0200

    add textPassive

--- app/src/main/java/com/cesar/lolchampinfo/presentation/screen/champion_details/ChampionDetailsScreen.kt
+++ app/src/main/java/com/cesar/lolchampinfo/presentation/screen/champion_details/ChampionDetailsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
--- app/src/main/java/com/cesar/lolchampinfo/presentation/screen/champion_details/components/ChampionPassive.kt
+++ app/src/main/java/com/cesar/lolchampinfo/presentation/screen/champion_details/components/ChampionPassive.kt
@@ -3,11 +3,13 @@ package com.cesar.lolchampinfo.presentation.screen.champion_details.components
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.cesar.lolchampinfo.data.repository.ApiRepositoryImpl
@@ -15,12 +17,20 @@ import com.cesar.lolchampinfo.domain.model.PassiveModel
 import com.cesar.lolchampinfo.domain.model.SpellModel
 
 @Composable
-fun ChampionPassive(passive: PassiveModel,
-                  modifier: Modifier = Modifier) {
+fun ChampionPassive(
+    passive: PassiveModel,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "Pasiva",
+        style = MaterialTheme.typography.titleLarge,
+        fontWeight = FontWeight.Bold
+    )
+
     ListItem(
         headlineContent = {
             Text(
-                text = passive.name ?:""
+                text = passive.name ?: ""
             )
         },
         supportingContent = {
@@ -33,7 +43,8 @@ fun ChampionPassive(passive: PassiveModel,
                 model = ApiRepositoryImpl.imagePassiveUrl + "${passive.image?.full}",
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(32.dp)
+                modifier = Modifier
+                    .size(32.dp)
                     .clip(RoundedCornerShape(8.dp))
             )
         }
